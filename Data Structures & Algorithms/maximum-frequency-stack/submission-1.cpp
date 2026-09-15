99f2d738b5bcf8c0c247c676e693f65fdfc74178
Shivansh Srivastava <theaverageguy19@gmail.com>
2026-09-15 22:35:18 +0530

    Add: maximum-frequency-stack - submission-1

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-1.cpp
@@ -0,0 +1,33 @@
+class FreqStack {
+public:
+    unordered_map<int, int> freq;       // value -> frequency
+    unordered_map<int, stack<int>> group; // frequency -> stack of values
+    int maxFreq = 0;
+
+    FreqStack() {
+    }
+
+    void push(int val) {
+        freq[val]++;
+
+        int f = freq[val];
+
+        group[f].push(val);
+
+        maxFreq = max(maxFreq, f);
+    }
+
+    int pop() {
+        int val = group[maxFreq].top();
+
+        group[maxFreq].pop();
+
+        freq[val]--;
+
+        if (group[maxFreq].empty()) {
+            maxFreq--;
+        }
+
+        return val;
+    }
+};
